8e1f9f8e64289848f45936141550cbcb6b3cbf82
MantasLo <62507428+MantasLo@users.noreply.github.com>
2020-04-21 21:09:16 +0300

    9

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MouseEvent } from '@agm/core';
 import { Router } from '@angular/router';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-footer',
