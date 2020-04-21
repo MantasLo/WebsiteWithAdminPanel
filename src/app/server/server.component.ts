2e566037c523b3f154f08801b12e3043bf086707
MantasLo <62507428+MantasLo@users.noreply.github.com>
2020-04-21 20:06:34 +0300

    3

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -22,7 +22,7 @@ export class ServerComponent implements OnInit {
   sum:number;
 
   constructor(private service: GalerijaService,
-    private fb: FormBuilder, private connectionService: MailserviceService, private route: ActivatedRoute) { 
+    private fb: FormBuilder, private connectionService: MailserviceService, private route: ActivatedRoute, private contactService: ContactService) { 
 
       this.contactForm = fb.group({
         'contactFormName': ['', Validators.required],
@@ -34,13 +34,18 @@ export class ServerComponent implements OnInit {
     }
 
   ngOnInit() {
-    if(this.route.snapshot.data['name'] == 'anketa'){
-      console.log("************************-------------------------***********************************");
-      var className = ".anketa";
-    const elementList = document.querySelectorAll(className);
-    const element = elementList[0] as HTMLElement;
-    element.scrollIntoView({ behavior: 'smooth' });
+    if(this.contactService.getContact() === true){
+      this.scroll();
     }
+    
+    
+//     if(this.route.snapshot.data['name'] == 'anketa'){
+//       console.log("************************-------------------------***********************************");
+//       var className = ".anketa";
+//     const elementList = document.querySelectorAll(className);
+//     const element = elementList[0] as HTMLElement;
+//     element.scrollIntoView({ behavior: 'smooth' });
+//     }
 
 
     this.number1 = Math.floor(Math.random() * (+this.max - +this.min)) + +this.min;
@@ -48,6 +53,15 @@ export class ServerComponent implements OnInit {
     this.number2 = Math.floor(Math.random() * (+this.max - +this.min)) + +this.min;
 
     this.sum = this.number1 + this.number2;
+
+  }
+  
+   scroll(){
+    var className = ".anketa";
+    const elementList = document.querySelectorAll(className);
+    const element = elementList[0] as HTMLElement;
+    element.scrollIntoView({ behavior: 'smooth' });
+    this.contactService.afterScroll();
 
   }
 
